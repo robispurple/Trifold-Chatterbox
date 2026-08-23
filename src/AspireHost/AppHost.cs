3f8036a53f739b33fad2c30cd7a5ca765d98a29b
Robert Norton <robertk.norton@gmail.com>
2026-08-22 23:07:06 -0400

    Add command to launch interactive terminals in Client.Spectre project

--- src/AspireHost/AppHost.cs
+++ src/AspireHost/AppHost.cs
@@ -5,6 +5,27 @@ var hubServer = builder.AddProject<Projects.HubServer>("hubserver")
 
 builder.AddProject<Projects.Client_Spectre>("client-spectre")
     .WithReference(hubServer)
-    .WaitFor(hubServer);
+    .WaitFor(hubServer)
+    .WithCommand(
+        name: "launch-3-terminals",
+        displayName: "Launch 3 Interactive Terminals",
+        executeCommand: _ =>
+        {
+            string scriptPath = Path.GetFullPath("../../scripts/launch-terminals.ps1");
+            var psi = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "pwsh.exe",
+                Arguments = $"-ExecutionPolicy Bypass -File \"{scriptPath}\"",
+                UseShellExecute = false
+            };
+            string userPath = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User) ?? "";
+            string machinePath = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine) ?? "";
+            psi.EnvironmentVariables["Path"] = $"{userPath};{machinePath}";
+
+            System.Diagnostics.Process.Start(psi);
+            return Task.FromResult(new ExecuteCommandResult { Success = true });
+        },
+        iconName: "WindowDevTools",
+        iconVariant: IconVariant.Filled);
 
 await builder.Build().RunAsync();
